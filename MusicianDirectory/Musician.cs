90cce03b59f425ae8f2503bba71d71ca37e3672b
Fabian Fabro <fabrofabian@gmail.com>
2019-09-09 10:44:08 -0700

    implemented MongoDB

---
+++ MusicianDirectory/Musician.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace MusicianDirectory
+{
+    class Musician
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Instrument { get; set; }
+
+        public Musician(string name, string instrument)
+        {
+            this.Name = name;
+            this.Instrument = instrument;
+        }
+    }
+}
--- MusicianDirectory/Program.cs
+++ MusicianDirectory/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace MusicianDirectory
 {
@@ -10,7 +12,30 @@ namespace MusicianDirectory
     {
         static void Main(string[] args)
         {
-            /*some changegg*/
+            Console.WriteLine("Welcome to the Musician's Directory!");
+            //Console.ReadKey();
+
+            var client = new MongoClient("mongodb://localhost:27017/MusicianDB");
+            var database = client.GetDatabase("MusicianDB");
+            Console.Out.WriteLine("Connection has been established.");
+
+            var musicianCollection = database.GetCollection<Musician>("Musicians", null);
+
+            database.DropCollection("Musicians");
+
+            //New entry
+            musicianCollection.InsertOne(new Musician("Fabian", "Laud"));
+
+            Musician gary = new Musician("Gary", "Flute");
+            Musician shara = new Musician("Shara", "Cello");
+            musicianCollection.InsertMany(new List<Musician> { gary, shara });
+
+            //count of existing records
+            int totalMusicians = musicianCollection.AsQueryable().Count();
+
+            Console.ReadLine();
+
         }
+
     }
 }
