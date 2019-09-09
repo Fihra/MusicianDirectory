00dad78ad1f04663d1d100d32584925717d5b54c
Fabian Fabro <fabrofabian@gmail.com>
2019-09-09 13:48:54 -0700

    Can retrieve objects from database

--- MusicianDirectory/Musician.cs
+++ MusicianDirectory/Musician.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MusicianDirectory
 {
     class Musician
     {
+        [BsonId]
         public ObjectId Id { get; set; }
+        [BsonElement("name")]
         public string Name { get; set; }
+        [BsonElement("instrument")]
         public string Instrument { get; set; }
+        [BsonElement("yearsOfExp")]
+        public int YearsOfExp { get; set; }
 
-        public Musician(string name, string instrument)
+        public Musician(string name, string instrument, int yearsOfExp)
         {
             this.Name = name;
             this.Instrument = instrument;
+            this.YearsOfExp = yearsOfExp;
         }
     }
 }
--- MusicianDirectory/Program.cs
+++ MusicianDirectory/Program.cs
@@ -10,31 +10,53 @@ namespace MusicianDirectory
 {
     class Program
     {
+        static async Task ShowNames(string[] args)
+        {
+            var client = new MongoClient();
+            IMongoDatabase db = client.GetDatabase("MusicianDB");
+            var collection = db.GetCollection<BsonDocument>("Musicians");
+
+            var list = await collection.Find(new BsonDocument()).ToListAsync();
+
+            foreach(var mus in list)
+            {
+                Console.WriteLine(mus);
+            }
+
+          
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Musician's Directory!");
-            //Console.ReadKey();
 
+            //Establish Connection to MusicianDB
             var client = new MongoClient("mongodb://localhost:27017/MusicianDB");
+            //Get Database, if it doesn't exist, create it
             var database = client.GetDatabase("MusicianDB");
-            Console.Out.WriteLine("Connection has been established.");
 
+            //Get Collection, it it doesn't exist, create it
             var musicianCollection = database.GetCollection<Musician>("Musicians", null);
 
+            //Drop Database
             database.DropCollection("Musicians");
 
             //New entry
-            musicianCollection.InsertOne(new Musician("Fabian", "Laud"));
+            musicianCollection.InsertOne(new Musician("Fabian", "Laud", 5));
 
-            Musician gary = new Musician("Gary", "Flute");
-            Musician shara = new Musician("Shara", "Cello");
+            //Multiple entries
+            Musician gary = new Musician("Gary", "Flute", 8);
+            Musician shara = new Musician("Shara", "Cello", 15);
             musicianCollection.InsertMany(new List<Musician> { gary, shara });
 
             //count of existing records
             int totalMusicians = musicianCollection.AsQueryable().Count();
 
-            Console.ReadLine();
+            Console.WriteLine("Amount of Musicians in this database: {0}", totalMusicians);
 
+            ShowNames(args).Wait();
+
+            Console.ReadLine();
         }
 
     }
