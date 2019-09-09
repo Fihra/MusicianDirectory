c7873bb846afcc971dadd71fbc5b18aeb097718b
Fabian Fabro <fabrofabian@gmail.com>
2019-09-09 16:59:37 -0700

    updated main menu

--- MusicianDirectory/Program.cs
+++ MusicianDirectory/Program.cs
@@ -32,23 +32,76 @@ namespace MusicianDirectory
 
         }
 
+        static void ViewMusicians()
+        {
+               
+        }
+
         static void MainMenu()
         {
+            Console.WriteLine("-----------------");
+            Console.WriteLine("[A]ll musicians");
+            Console.WriteLine("[N]ew musician");
+            Console.WriteLine("[V]iew musician");
+            Console.WriteLine("[U]pdate musician info");
+            Console.WriteLine("[R]emove musician");
+            Console.WriteLine("[S]earch directory");
+            Console.WriteLine("[Q]uit");
+            Console.Write("Enter Choice: ");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Musician's Directory!");
+
+            var connectionString = "mongodb://localhost:27017/MusicianDB";
+            //Establish Connection to MusicianDB
+            var client = new MongoClient(connectionString);
+            //Get Database, if it doesn't exist, create it
+            var database = client.GetDatabase("MusicianDB");
+
+            //Get Collection, it it doesn't exist, create it
+            //var musicianCollection = database.GetCollection<Musician>("Musicians", null);
+
+            //Drop Database
+            //database.DropCollection("Musicians");
+
+            //New entry
+            //musicianCollection.InsertOne(new Musician("Fabian", "Laud", 5));
+
+            //Multiple entries
+            //Musician gary = new Musician("Gary", "Flute", 8);
+            //Musician shara = new Musician("Shara", "Cello", 15);
+            //musicianCollection.InsertMany(new List<Musician> { gary, shara });
+
+            //count of existing records
+            //int totalMusicians = musicianCollection.AsQueryable().Count();
+
+            //Console.WriteLine("Amount of Musicians in this database: {0}", totalMusicians);
+
             bool isLooping = true;
             do
             {
-                Console.WriteLine("-----------------");
-                Console.WriteLine("[V]iew all musicians");
-                Console.WriteLine("[S]earch directory");
-                Console.WriteLine("[Q]uit");
-                Console.Write("Enter Choice: ");
+                MainMenu();
                 string userInput = Console.ReadLine();
                 Console.WriteLine("-----------------");
 
                 switch (userInput.ToLower())
                 {
+                    case "a":
+                        Console.WriteLine("All musicians");
+                        break;
+                    case "n":
+                        Console.WriteLine("New musician form");
+                        break;
                     case "v":
-                        Console.WriteLine("Viewing all musicians");
+                        Console.WriteLine("View musician");
+                        break;
+                    case "u":
+                        Console.WriteLine("Update musician Info");
+                        break;
+                    case "r":
+                        Console.WriteLine("Remove Musician");
                         break;
                     case "s":
                         Console.WriteLine("Searching directory");
@@ -60,39 +113,9 @@ namespace MusicianDirectory
                     default:
                         Console.WriteLine("Invalid input");
                         break;
-                }
-            } while (isLooping);
+                    }
+                } while (isLooping);
 
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the Musician's Directory!");
-            //Establish Connection to MusicianDB
-            var client = new MongoClient("mongodb://localhost:27017/MusicianDB");
-            //Get Database, if it doesn't exist, create it
-            var database = client.GetDatabase("MusicianDB");
-
-            //Get Collection, it it doesn't exist, create it
-            var musicianCollection = database.GetCollection<Musician>("Musicians", null);
-
-            //Drop Database
-            database.DropCollection("Musicians");
-
-            //New entry
-            musicianCollection.InsertOne(new Musician("Fabian", "Laud", 5));
-
-            //Multiple entries
-            Musician gary = new Musician("Gary", "Flute", 8);
-            Musician shara = new Musician("Shara", "Cello", 15);
-            musicianCollection.InsertMany(new List<Musician> { gary, shara });
-
-            //count of existing records
-            int totalMusicians = musicianCollection.AsQueryable().Count();
-
-            //Console.WriteLine("Amount of Musicians in this database: {0}", totalMusicians);
-
-            MainMenu();
             Console.WriteLine("Thank you for using the MusicianDirectory");
             //ShowNames(args).Wait();
 
